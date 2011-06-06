02fa94bf6923143a599aa937cdc17d1e03367139
Paul Biggar <paul.biggar@gmail.com>
2011-06-05 18:51:57 -0700

    Add error checking.

--- hackerite/lib/main.js
+++ hackerite/lib/main.js
@@ -4,9 +4,11 @@ const notificationBox = require("notification-box");
 
 tabs.on("ready", function onReady(tab) {
   get_ids(tab.url, function (ids) {
-    get_data(ids[0], function (post) {
-      var nb = notificationBox.NotificationBox(post.title, "id", "label");
-    });
+    if (ids.length > 0) {
+      get_data(ids[0], function (post) {
+        var nb = notificationBox.NotificationBox(post.title, "id", "label");
+      });
+    }
   });
 });
 
@@ -21,6 +23,13 @@ function get_data(id, callback) {
 function req(url, callback) {
   request.Request({
     url: url,
-    onComplete: function (response) { callback(response.json); },
+    onComplete: function (response) {
+      if (response.status == 200) {
+        callback(response.json);
+      }
+      else {
+        console.log('Error (' + response.status + ') in response: ' + response.text);
+      }
+    },
   }).get();
 }
