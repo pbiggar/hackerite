d21bff438496322d55e93664875a94650ba356c6
Paul Biggar <paul.biggar@gmail.com>
2011-06-05 17:34:24 -0700

    Make the notification box appear after only a page is ready.

--- hackerite/lib/main.js
+++ hackerite/lib/main.js
@@ -1,16 +1,7 @@
-const widgets = require("widget");
 const tabs = require("tabs");
 const notificationBox = require("notification-box");
 
-var widget = widgets.Widget({
-  id: "mozilla-link",
-  label: "Mozilla website",
-  contentURL: "http://www.mozilla.org/favicon.ico",
-  onClick: function() {
-    tabs.open("http://www.mozilla.org/");
-  }
+tabs.on("ready", function onReady(tab) {
+  var nb = notificationBox.NotificationBox(tab.url, "id", "label");
 });
 
-console.log("The add-on is running.");
-
-var nb = notificationBox.NotificationBox("message", "id", "label");
