c55faba35653614310bc47e12a98a0d75d849fab
Paul Biggar <paul.biggar@gmail.com>
2011-06-05 18:44:01 -0700

    Get the title of the post into the notification bar.

--- hackerite/lib/main.js
+++ hackerite/lib/main.js
@@ -1,7 +1,26 @@
 const tabs = require("tabs");
+const request = require("request");
 const notificationBox = require("notification-box");
 
 tabs.on("ready", function onReady(tab) {
-  var nb = notificationBox.NotificationBox(tab.url, "id", "label");
+  get_ids(tab.url, function (ids) {
+    get_data(ids[0], function (post) {
+      var nb = notificationBox.NotificationBox(post.title, "id", "label");
+    });
+  });
 });
 
+function get_ids(url, callback) {
+  req('http://api.ihackernews.com/getid?url=' + encodeURI(url), callback);
+}
+
+function get_data(id, callback) {
+  req('http://api.ihackernews.com/post/' + id, callback);
+}
+
+function req(url, callback) {
+  request.Request({
+    url: url,
+    onComplete: function (response) { callback(response.json); },
+  }).get();
+}
